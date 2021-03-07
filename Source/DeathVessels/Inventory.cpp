033fb59b9c4aa80dae017b44bc8da49e8ba197f7
UnitedMonarchy <32991839+UnitedMonarchy@users.noreply.github.com>
2021-03-06 21:24:27 -0700

    Inventory Modifications
    Modified all item functions related to inventory in order to make them be more widely used instead of just for the ammo calculations and such. Still need to implement adding onto a value such after opening the inventory. Also need to cleanup items in the array that reach 0 and then reset the ui to look how it did originally.

--- Source/DeathVessels/Inventory.cpp
+++ Source/DeathVessels/Inventory.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Inventory.h"
+#include "InteractionComponent.h"
 
 
 // Sets default values for this component's properties
@@ -37,14 +38,16 @@ void UInventoryTest::AddAmmo(class AMyCharacter* Player)
 		//if it is unique then allow it
 		//check index before to see if it stays the same if it stays the same return false so it doesn't make that widget
 		ItemIndex = ItemName.Num(); 
-		if(Player->Ammo)
+		if(Player->Ammo > 0)
 		{
 			ItemName.AddUnique("Ammo");
+			ItemDescription.AddUnique("5.56 Ammo, goes very fast and hurts.");
 			ItemNum.Add(Player->Ammo);
 		}
 		if(Player->Wood > 0)
 		{
 			ItemName.AddUnique("Wood");
+			ItemDescription.AddUnique("Pulled straight off of a tree or something. Would make a good wacking stick.");
 			ItemNum.Add(Player->Wood);
 		}
 		ValueSet = true;
@@ -57,13 +60,15 @@ void UInventoryTest::CreateItemWidget(int32& ItemsInInventory)
 	ItemsInInventory = ItemName.Num() - 1; //so you can create the widget that many times, index number that way you can get the amount and title for each
 }
 
-void UInventoryTest::ItemInfo(class AMyCharacter* Player, int32& Amount, FString& Title)
+void UInventoryTest::ItemInfo(class AMyCharacter* Player, int32& Amount, FString& Title, FString& Description)
 {
 	//basically you could make an array, add in whatever item get in your inventory, then you have all the data of what is in the players inventory,
 	// when it comes to the player removing something from their inventory what you would do is decrease the indexer.
 	//(Which is just increasing the a value by one to get the correct index of what to add in next to the array.)
 
 	//you just get the array, here and set the value from the array to either amount or title
+	
+	//I should be able to just use this
 	if(ItemIndex == ItemName.Num())
 	{
 		ItemIndex = 0;
@@ -72,30 +77,93 @@ void UInventoryTest::ItemInfo(class AMyCharacter* Player, int32& Amount, FString
 	{
 		Amount = ItemNum[ItemIndex];
 		Title = ItemName[ItemIndex];
+		Description = ItemDescription[ItemIndex];
 		ItemIndex++;
 	}
 }
 
-void UInventoryTest::DropItem(class AMyCharacter* Player, int32 RemovingAmmo)
+void UInventoryTest::DropItem(class AMyCharacter* Player, int32 AmountToRemove)
 {
-	if(Player->Ammo > 0 && Player != nullptr && !Player->HasAuthority() && Player->Ammo - RemovingAmmo  > 0 && RemovingAmmo > 0)
+	//get index of whatever item you have then change the value in it in order to make the inventory accurate.
+	ItemTypeName = "Null";
+	if(AmountToRemove <= 0 && ValueSet){return;}
+
+	if(Player->Ammo - AmountToRemove  >= 0)
 	{
-		Player->Ammo -= RemovingAmmo;
+		
+		ItemTypeName = "5.56 Ammo";
+		Player->Ammo -= AmountToRemove;
+		
+		for(int32 i = 0; ItemName[i]  == "5.56 Ammo" && i < ItemName.Num(); i++)
+		{
+			ItemNum[i] = Player->Ammo;
+			ItemTypeIndex = i;
+			
+		}
+		if(ItemName[0] == "5.56 Ammo")
+		{
+			ItemNum[0] = Player->Wood;
+		}
+	}
+	else if(Player->Wood - AmountToRemove >= 0 )
+	{
+		
+		ItemTypeName = "Wood";
+		Player->Wood -= AmountToRemove;
+
+		for(int32 i = 0; ItemName[i]  == "Wood" && i < ItemName.Num() - 1; i++)
+		{
+			ItemNum[i] = Player->Wood;
+			ItemTypeIndex = i;
+		}
+		if(ItemName[0] == "Wood")
+		{
+			ItemNum[0] = Player->Wood;
+		}
+
+		
+		if(Player->Wood == 0 )
+		{
+			ItemNum.RemoveAt(ItemTypeIndex);
+			ItemName.RemoveAt(ItemTypeIndex);
+			ItemDescription.RemoveAt(ItemTypeIndex);
+		}
+	}
+
+	if(ItemTypeName != "Null")	
+	{
 		ServerDropItem(Player, Player->GetActorLocation(), Player->GetActorRotation());
-		UE_LOG(LogTemp, Warning, TEXT("occured"))
 	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Player does not have ammo to drop"))
-	}
+	
+	
 }
 
 void UInventoryTest::ServerDropItem_Implementation(class AMyCharacter* Player, FVector ClientLocation, FRotator ClientRotation)
 {
-	AmmoModel = GetWorld()->SpawnActor<AActor>(AmmoModelClass, ClientLocation, ClientRotation);
+	if(AmmoModelClass != nullptr)
+	{
+
+		ItemModel = GetWorld()->SpawnActor<AActor>(AmmoModelClass, ClientLocation, ClientRotation);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No model set for ammo"))
+	}
 }
 
-float UInventoryTest::AmmoToRemove(class AMyCharacter* Player, float ValueToRemove)
+float UInventoryTest::AmountToRemove(class AMyCharacter* Player, float ValueToRemove, float Amount)
 {
-	return Player->Ammo * ValueToRemove;
+	return Amount * ValueToRemove;
+}
+
+void UInventoryTest::ItemAdjustment_BP(class AMyCharacter* Player, UStaticMeshComponent* ItemMesh, UInteractionComponent* InteractionComponent)
+{
+	if(ItemMesh == nullptr || InteractionComponent == nullptr){return;}
+	//can probably remove ammo and stuff here that way you are not dealing with it elsewhere
+	if(ItemTypeName == "5.56 Ammo")
+	{
+		ItemMesh->SetStaticMesh(Ammo);
+		InteractionComponent->SetInteractableNameText(FText::FromString("5.56 Ammo"));
+	}
+	ItemMesh->AddImpulse(FVector(0, 150, 0));
 }
--- Source/DeathVessels/Inventory.h
+++ Source/DeathVessels/Inventory.h
@@ -21,19 +21,25 @@ public:
 	void AddAmmo(class AMyCharacter* Player);
 
 	UFUNCTION(BlueprintCallable)
+	void ItemAdjustment_BP(class AMyCharacter* Player, UStaticMeshComponent* ItemMesh, UInteractionComponent* InteractionComponent);
+
+	UFUNCTION(BlueprintCallable)
 	void CreateItemWidget(int32& ItemsInInventory);
 
 	UFUNCTION(BlueprintCallable)
-	void ItemInfo(class AMyCharacter* Player, int32& Amount, FString& Title);
+	void ItemInfo(class AMyCharacter* Player, int32& Amount, FString& Title, FString& Description);
 	//class AMyCharacter* Player;
 	UFUNCTION(BlueprintCallable)
-	void DropItem(class AMyCharacter* Player, int32 RemovingAmmo);
+	void DropItem(class AMyCharacter* Player, int32 AmountToRemove);
 
 	UFUNCTION(BlueprintCallable)
-	float AmmoToRemove(class AMyCharacter* Player, float ValueToRemove);
+	float AmountToRemove(class AMyCharacter* Player, float ValueToRemove, float Amount);
 
 	UPROPERTY(BlueprintReadWrite)
 	bool AddToInventory;
+
+	UPROPERTY(EditAnywhere, Category = "Items")
+	UStaticMesh* Ammo;
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -46,13 +52,19 @@ private:
 	bool ValueSet;
 
 	TArray<FString> ItemName;
+	TArray<FString> ItemDescription;
 	TArray<int32> ItemNum;
 
 	UFUNCTION(Server, Unreliable)
 	void ServerDropItem(class AMyCharacter* Player, FVector ClientLocation, FRotator ClientRotation);
 
-	AActor* AmmoModel;
+	AActor* ItemModel;
+
+	FString ItemTypeName;
 
 	int32 ItemIndex = 0;
+
+	int32 ItemTypeIndex = 0;
+
 
 };
